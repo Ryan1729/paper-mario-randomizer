f5a3a02631e169e56fa78944a3ce8499ca30a275
Ryan1729 <ryan1729@gmail.com>
2019-11-05 03:10:25 -0700

    make sure you can get the hammer in the most direct way

--- src/main.rs
+++ src/main.rs
@@ -60,29 +60,53 @@ struct Room {
     warp_ptrs: Vec<u32>,
 }
 
+macro_rules! d {
+    () => {
+        Default::default();
+    };
+    (for $type: ty : $value: expr) => {
+        impl Default for $type {
+            fn default() -> Self {
+                $value
+            }
+        }
+    };
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input_path = "./Paper Mario (USA).z64";
     let output_path = "./Paper Mario (USA) Shuffled.z64";
 
-    enum Mode {
+    enum StartMode {
         Default,
-        QuickStart
+        Quick
     }
+    d!(for StartMode : StartMode::Default);
+    enum ItemMode {
+        Default,
+        Shuffle
+    }
+    d!(for ItemMode : ItemMode::Default);
 
     let mut args = std::env::args();
     //exe name
     args.next();
 
-    let mode = match args.next() {
-        Some(s) => {
-            let s: &str = &s;
-            match s {
-                "--quick-start"|"-q" => Mode::QuickStart,
-                _ => Mode::Default
+    let mut start = d!();
+    let mut item = d!();
+
+    while let Some(s) = args.next() {
+        let s: &str = &s;
+        match s {
+            "--quick-start" => start = StartMode::Quick,
+            "--shuffle-items" => item = ItemMode::Shuffle,
+
+            _ => {
+                eprintln!("unknown arg {:?}", s);
+                std::process::exit(1)
             }
         }
-        _ => Mode::Default
-    };
+    }
 
     fs::copy(input_path, output_path)?;
 
@@ -150,27 +174,50 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         offset = 0x168074;
         seek_write_advance!(0x2406FF81);
 
-        if let Mode::QuickStart = mode {
+        if let StartMode::Quick = start {
             // don't start the game from Mario's house
             offset = 0x168080;
             seek_write_advance!(0x24020000);
         }
     }
 
+    macro_rules! read_u32 {
+        () => {{
+            let mut buf = [0u8; 4];
+            output.read_exact(&mut buf)?;
+            u32::from_be_bytes(buf)
+        }};
+    }
+
     let room_base_ptr: u32 = 0x80240000;
+
+    // Set the pointed to exit in the pointed to room at the target room and entrance
+    macro_rules! write_out_room_entrance {
+        (
+            $room_ptr: expr,
+            $warp_ptr: expr,
+            $target_room_name: expr,
+            $target_room_entrance: expr
+        ) => {
+            let room_ptr: u32 = $room_ptr;
+            let warp_ptr: u32 = $warp_ptr;
+            let target_room_name: &str = $target_room_name;
+            let target_room_entrance: u32 = $target_room_entrance;
+            output.seek(SeekFrom::Start((room_ptr + warp_ptr - room_base_ptr + 0xC) as _))?;
+            let warp_room_ptr = read_u32!();
+            output.write(&target_room_entrance.to_be_bytes())?;
+            output.seek(SeekFrom::Start((room_ptr + warp_room_ptr - room_base_ptr) as _))?;
+            output.write(CString::new(target_room_name)?.as_bytes_with_nul())?;
+        };
+    }
+
     let room_count = 421;
 
     let xs: &mut Xs = &mut [Wrapping(42),Wrapping(42),Wrapping(42),Wrapping(42)];
-    for i in 0..room_count {
-        output.seek(SeekFrom::Start(0x6B450 + i*0x20))?;
 
-        macro_rules! read_u32 {
-            () => {{
-                let mut buf = [0u8; 4];
-                output.read_exact(&mut buf)?;
-                u32::from_be_bytes(buf)
-            }};
-        }
+    for i in 0..room_count {
+        output.seek(SeekFrom::Start(0x6B450 + i * 0x20))?;
+
         let name_ptr = read_u32!() - 0x80024C00;
 
         output.seek(SeekFrom::Current(4))?;
@@ -193,27 +240,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
         let name = name_buf.to_str()?;
 
-        for warp_ptr in room_data[name].warp_ptrs.iter() {
+        dbg!(i, name, room_ptr);
+
+        for &warp_ptr in room_data[name].warp_ptrs.iter() {
             let rand_room = xs_choice_str(xs, &room_names);
             let rand_entrance = xs_choice(xs, &room_data[rand_room].entrances);
 
-            output.seek(SeekFrom::Start((room_ptr + warp_ptr - room_base_ptr + 0xC) as _))?;
-            let warp_room_ptr = read_u32!();
-            output.write(&rand_entrance.to_be_bytes())?;
-            output.seek(SeekFrom::Start((room_ptr + warp_room_ptr - room_base_ptr) as _))?;
-            output.write(CString::new(rand_room)?.as_bytes_with_nul())?;
+            write_out_room_entrance!(room_ptr, warp_ptr, rand_room, rand_entrance);
         }
 
-        for item_ptr in room_data[name].items.iter() {
-            output.seek(SeekFrom::Start((room_ptr + item_ptr - room_base_ptr) as _))?;
-            let rand_item = xs_u32(xs, 1, 0x16C);
-            let read_u32 = read_u32!();
-            if 0 < read_u32 && read_u32 < 0x200 {
-                output.seek(SeekFrom::Current(-4))?;
-                output.write(&rand_item.to_be_bytes())?;
+        if let ItemMode::Shuffle = item {
+            for item_ptr in room_data[name].items.iter() {
+                output.seek(SeekFrom::Start((room_ptr + item_ptr - room_base_ptr) as _))?;
+                let rand_item = xs_u32(xs, 1, 0x16C);
+                let read_u32 = read_u32!();
+                if 0 < read_u32 && read_u32 < 0x200 {
+                    output.seek(SeekFrom::Current(-4))?;
+                    output.write(&rand_item.to_be_bytes())?;
+                }
             }
         }
     }
+
+    // start by the hammer by making "kmr_00" (Mario's fall area) lead there.
+    write_out_room_entrance!(0x8ABF90, 2149846604, "kmr_04", 02);
+    write_out_room_entrance!(0x8ABF90, 2149854336, "kmr_04", 02);
+
 
     output.sync_data()?;
     drop(output);
